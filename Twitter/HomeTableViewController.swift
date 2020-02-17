5889076656516ccb9cb62e7084d730c341546c62
smc <cooper.saraah@gmail.com>
2020-02-16 20:16:55 -0500

    retweet successful

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -113,9 +113,10 @@ class HomeTableViewController: UITableViewController {
             cell.profileImageView.image = UIImage(data: imageData)
         }
         
-    
+    // get this info from the twitter array (reference by its key-value pair)
         cell.setFavorite(tweetArray[indexPath.row]["favorited"] as! Bool)
         cell.tweetId = tweetArray[indexPath.row]["id"] as! Int
+        cell.setRetweeted(tweetArray[indexPath.row]["retweeted"] as! Bool)
         
         return cell
     }
